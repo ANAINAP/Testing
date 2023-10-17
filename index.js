97fecefb11ddf58dadb19b7989566435a748f514
ANAINAP <anainarajeev3@gmail.com>
2023-10-17 20:42:42 +0530

    4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/',(req, res)=>{
 
 app.listen(4000 ,(error) =>{ 
 	if(!error) 
-		console.log("Server is Successfully Running , and App is listening on port "+ process.env.PORT) 
+		console.log("Server is Successfully Running , and App is listening on port "+ 4000) 
 	else
 		console.log("Error occurred, server can't start", error); 
 	} 
